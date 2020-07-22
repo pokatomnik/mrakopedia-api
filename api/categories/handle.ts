f1995227d6eb91b063173917c803da6ffb9245e2
Danilian Akhmedzianov <hugefast@gmail.com>
2020-07-23 01:29:25 +0300

    add exclude pages

--- api/categories/handle.ts
+++ api/categories/handle.ts
@@ -8,6 +8,23 @@ interface ICategoryEntry {
   '*': string;
 }
 
+// This are the hardcoded page titles which should not be sent
+const exceptions = [
+  'крипи',
+  'шаблоны',
+  'шаблоны:внутренние ссылки',
+  'шаблоны:форматирование',
+  'шаблоны для документирования шаблонов',
+  'википедия:шаблоны для документирования шаблонов',
+  'шаблоны для шаблонов',
+  'документация шаблонов',
+  'страницы, использующие повторяющиеся аргументы в вызовах шаблонов',
+  'страницы со слишком большим количеством вызовов ресурсоёмких функций',
+  'страницы, использующие недопустимые самозакрывающиеся html-теги',
+  'страницы с неработающими файловыми ссылками',
+  'страницы с опросами',
+];
+
 export const handle = (_: unknown, response: NowResponse) => {
   const params: Record<string, string> = {
     action: 'query',
@@ -23,8 +40,14 @@ export const handle = (_: unknown, response: NowResponse) => {
     never,
     AxiosResponse<IWikiQueryResponse<'allcategories', Array<ICategoryEntry>>>
   >(`${MRAKOPEDIA_API_ENDPOINT}?${searchString}`)
-    .then((res) =>
-      res.data.query.allcategories.map(({ '*': entry }) => ({
+    .then((res) => res.data.query.allcategories.map(({ '*': entry }) => entry))
+    .then((entryNames) =>
+      entryNames.filter(
+        (entryName) => !exceptions.includes(entryName.toLocaleLowerCase())
+      )
+    )
+    .then((entries) =>
+      entries.map((entry) => ({
         title: entry,
         url: `/api/categories/${encodeURIComponent(entry)}`,
       }))
