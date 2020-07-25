3c258109854eb18dc37f1c4eeb3ee99ffce399d7
Danilian Akhmedzianov <hugefast@gmail.com>
2020-07-25 03:01:35 +0300

    add histories of the month endpoint

---
+++ api/hotm/handler.ts
@@ -0,0 +1,44 @@
+import { NowResponse } from '@vercel/node';
+import { wiki } from '../../app/Wiki';
+import { HISTORY_OF_THE_MONTH_PAGE_NAME } from '../../app/constants';
+import { Error, makePageResponse } from '../../app/utils';
+
+type InferPromise<T> = T extends PromiseLike<infer U> ? U : T;
+
+type Page = InferPromise<ReturnType<typeof wiki['page']>>;
+
+const PAGE_FETCH_ERROR = Error(
+  'FAILED_TO_GET_PAGE_WITH_HISTORIES',
+  'Failed to get page with histories of the month'
+);
+const LINKS_FETCH_ERROR = Error(
+  'FAILED_TO_GET_LINKS',
+  'Failed to get history of the month links'
+);
+
+export const handler = async (_: unknown, response: NowResponse) => {
+  let page: Page | null = null;
+
+  try {
+    page = await wiki.page(HISTORY_OF_THE_MONTH_PAGE_NAME);
+  } catch {
+    response.status(500).json(PAGE_FETCH_ERROR);
+    return;
+  }
+
+  if (page === null) {
+    response.status(500).json(PAGE_FETCH_ERROR);
+    return;
+  }
+
+  let links: Array<string> = [];
+
+  try {
+    links = await page.links();
+  } catch {
+    response.status(500).json(LINKS_FETCH_ERROR);
+    return;
+  }
+
+  response.json(links.map(makePageResponse));
+};
--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,3 @@
 export const MRAKOPEDIA_API_ENDPOINT = 'https://mrakopedia.net/w/api.php';
+export const HISTORY_OF_THE_MONTH_PAGE_NAME =
+  'Мракопедия:Истории_с_главной_страницы';
