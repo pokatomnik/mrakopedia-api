b5cb23d2921aa5c08be037ae1626979167333658
Danilian Akhmedzianov <hugefast@gmail.com>
2020-07-30 00:21:18 +0300

    dark theme support

--- app/page.ts
+++ app/page.ts
@@ -37,7 +37,9 @@ export class Page {
       <html>
           <head>
               <meta charset="utf-8" />
-              <link rel="stylesheet" href="/static/style.css" />
+              <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap-reboot.css" />
+              <link rel="stylesheet" href="/static/overrides.css" />
+              <link rel="stylesheet" href="/static/device-theme.css" />
               <title>${this.title}</title>
               <style>
                   ${this.ignoreList.join(', ')} {
