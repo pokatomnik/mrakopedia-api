7bc7146e1eb2816d2a87914f53c0ed225823d347
Danilian Akhmedzianov <hugefast@gmail.com>
2020-07-30 23:42:20 +0300

    decrease functions number

--- api/random/index.ts
+++ api/random/index.ts
@@ -1,2 +1,24 @@
-import { handle } from './handle';
-export default handle;
+import { NowResponse } from '@vercel/node';
+import { wiki } from '../../app/Wiki';
+import { Error, makePageResponse } from '../../app/utils';
+
+export default async (_: unknown, response: NowResponse) => {
+  let randomPageTitles: Array<string> | undefined = undefined;
+  try {
+    randomPageTitles = await wiki.random(1);
+  } catch {
+    response
+      .status(500)
+      .json(Error('FAILED_GET_RANDOM_PAGE', 'Unable to get random page'));
+    return;
+  }
+
+  if (randomPageTitles.length < 1) {
+    response.status(404).json('No pages to get');
+    return;
+  }
+
+  const firstRandom = randomPageTitles[0];
+
+  response.json(makePageResponse(firstRandom));
+};
