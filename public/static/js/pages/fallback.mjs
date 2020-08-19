31f7592fb7102a9a225bebb80595b495f53c5c3a
Danilian Akhmedzianov <hugefast@gmail.com>
2020-08-20 01:13:12 +0300

    add fancy 404 page

--- public/static/js/pages/fallback.mjs
+++ public/static/js/pages/fallback.mjs
@@ -1,5 +1,21 @@
-import { html } from '../preact/preact.mjs';
+import Preact, { html } from '../preact/preact.mjs';
+import { Main } from '../components/main.mjs';
+import { Header } from '../components/header.mjs';
+import { Container } from '../components/container.mjs';
+import { Jumbotron } from '../components/jumbotron.mjs';
 
 export const Fallback = () => {
-  return html`<span>404</span>`;
+  return html`
+    <${Preact.Fragment}>
+    <${Header} />
+    <${Main}>
+      <${Jumbotron}>
+        <${Container}>
+          <h1>404</h1>
+          <p>Страница не найдена.</p>
+        </${Container}>
+      </${Jumbotron}>
+    </${Main}>
+    </${Preact.Fragment}>
+  `;
 };
