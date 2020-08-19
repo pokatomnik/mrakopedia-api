4da8e430f627468f6170bb32b4b80ff7d48a9a08
Danilian Achmedzianov <hugefast@gmail.com>
2020-08-19 14:54:02 +0300

    Update preact.mjs.d.ts

--- public/static/js/preact/preact.mjs.d.ts
+++ public/static/js/preact/preact.mjs.d.ts
@@ -1,7 +1,7 @@
 import Preact from 'preact';
 import _Hooks from 'preact/hooks';
 
-export type Element = typeof Preact.createElement;
+export type Element = ReturnType<typeof Preact.createElement>;
 export const Hooks = _Hooks;
 export default Preact;
 export function html(
