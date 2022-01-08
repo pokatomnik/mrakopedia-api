32fd2f6513a732344d8df86db86cadf263ab540b
Danilian Akhmedzianov <hugefast@gmail.com>
2022-01-08 17:58:06 +0300

    add eols

--- app/config.ts
+++ app/config.ts
@@ -2,6 +2,6 @@ export const CONFIGURATION = {
   contacts: {
     issues: 'https://github.com/pokatomnik/mrakopedia-reader/issues',
     telegram: 'https://t.me/johndoefoobar',
-    mail: 'hugefast@gmail.com'
-  }
+    mail: 'hugefast@gmail.com',
+  },
 };
