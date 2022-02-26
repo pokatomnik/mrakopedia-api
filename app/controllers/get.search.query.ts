a7cd58b05a024fffb3a9c5e601d40fd386afd623
Danilian Akhmedzianov <hugefast@gmail.com>
2022-02-27 01:13:12 +0300

    implement searching by text

--- app/controllers/get.search.query.ts
+++ app/controllers/get.search.query.ts
@@ -1,25 +1,52 @@
 import { NowRequest, NowResponse } from '@vercel/node';
-import { stringify, Error, makePageResponse, makeSortFnBy } from '../utils';
+import { stringify, Error, makePageResponse, makeSearch } from '../utils';
 import { wiki } from '../Wiki';
+import Axios, { AxiosResponse } from 'axios';
+import { IWikiQueryResponse } from '../IWikiQueryResponse';
+import { MRAKOPEDIA_API_ENDPOINT } from '../constants';
+import uniqBy from 'lodash/uniqBy';
+import identity from 'lodash/identity';
 
-export const searchByString = (req: NowRequest, res: NowResponse) => {
+function searchByTitle(query: string): Promise<Array<string>> {
+  return wiki.search(query).then((result) => result.results);
+}
+
+async function searchByText(query: string): Promise<Array<string>> {
+  const params: Record<string, string> = {
+    action: 'query',
+    list: 'search',
+    srwhat: 'text',
+    format: 'json',
+    srlimit: '500',
+    srsearch: encodeURIComponent(query),
+  };
+  const apiQuery = makeSearch(params);
+  const result = await Axios.get<
+    unknown,
+    AxiosResponse<IWikiQueryResponse<'search', Array<{ title: string }>>>
+  >(`${MRAKOPEDIA_API_ENDPOINT}?${apiQuery}`);
+
+  return result.data.query.search.map((it) => it.title);
+}
+
+export async function searchByString(req: NowRequest, res: NowResponse) {
   const query = stringify(req.query.query);
   if (!query) {
     return res.json([]);
   }
 
-  wiki
-    .search(query)
-    .then((result) => result.results)
-    .then((titles) => {
-      return titles.map(makePageResponse);
-    })
-    .then((results) => {
-      return res.json(results.sort(makeSortFnBy('title')));
-    })
-    .catch(() => {
-      return res
-        .status(500)
-        .json(Error('FAILED_SEARCH_RESPONSE', 'Search failed'));
-    });
-};
+  try {
+    const [pagesByTitle, pagesByText] = await Promise.all([
+      searchByTitle(query),
+      searchByText(query),
+    ]);
+
+    const joinedResults = uniqBy(pagesByTitle.concat(pagesByText), identity);
+    const pages = joinedResults.map(makePageResponse);
+    return res.json(pages);
+  } catch (e) {
+    return res
+      .status(500)
+      .json(Error('FAILED_SEARCH_RESPONSE', 'Search failed'));
+  }
+}
